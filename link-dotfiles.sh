4e34ab1c9e63d3ded9237161d16ef1b0d405cca7
Matthew Bilyeu <mbilyeu@rjmetrics.com>
2016-06-16 16:26:01 -0400

    Prompt and force hard link

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-ln ./.spacemacs ~/.spacemacs
+read -p "This will overwrite any existing ~/.spacemacs file. Continue? [Y/n] " answer
+
+while true
+do
+  case $answer in
+   [Y]* ) ln -f ./.spacemacs ~/.spacemacs
+          echo "./.spacemacs has been hard-linked to ~/.spacemacs"
+          break;;
+
+   * ) echo "did nothing"
+       exit;;
+  esac
+done
+
