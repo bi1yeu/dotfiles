05a4703b570ecea0354bd66504de7e38ef21efc9
Matthew Bilyeu <bilyeu.matt@gmail.com>
2016-10-02 13:16:21 -0400

    Support calling from different dir

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 read -p "This will overwrite any existing ~/.spacemacs file. Continue? [Y/n] " answer
-
+SCRIPT_PATH="${BASH_SOURCE[0]}";
 while true
 do
   case $answer in
-   [Y]* ) ln -f ./.spacemacs ~/.spacemacs
-          echo "./.spacemacs has been hard-linked to ~/.spacemacs"
+   [Y]* ) ln -f `dirname ${SCRIPT_PATH}`/.spacemacs ~/.spacemacs
+          echo "`dirname ${SCRIPT_PATH}`/.spacemacs has been hard-linked to ~/.spacemacs"
           break;;
 
    * ) echo "did nothing"
