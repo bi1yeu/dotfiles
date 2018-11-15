bd3e00998f5e6968b93883c15a89411ef3d3a228
Matthew Bilyeu <matt@stitchdata.com>
2018-11-15 17:49:10 -0500

    bypass prompt

--- link-dotfiles.bash
+++ link-dotfiles.bash
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-read -rp "# Are you sure you want to symlink this repo's spacemacs directory to ~/.spacemacs.d? [y/N] " confirm
-pushd `dirname $0` > /dev/null
-SCRIPTPATH=`pwd -P`
-popd > /dev/null
+if [[ "$1" == "-y" ]]
+then
+  confirm=y
+else
+  read -rp "# Are you sure you want to symlink this repo's spacemacs directory to ~/.spacemacs.d? [y/N] " confirm
+  pushd `dirname $0` > /dev/null
+  SCRIPTPATH=`pwd -P`
+  popd > /dev/null
+fi
 
 if [[ $confirm == y ]]
 then
