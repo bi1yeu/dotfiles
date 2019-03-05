eed99b95b7a6018b3e21dfa9fdd13dfb9526ce35
Matthew Bilyeu <matt@stitchdata.com>
2019-03-05 14:37:50 -0500

    `org-insert-src-block` can insert source block w/o code specified

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -349,7 +349,7 @@ before packages are loaded. If you are unsure, you should try in setting them in
   "Insert a `SRC-CODE-TYPE' type source code block in org-mode."
   (interactive
    (let ((src-code-types
-          '("emacs-lisp" "python" "C" "sh" "java" "js" "json" "clojure" "coffee"
+          '("" "emacs-lisp" "python" "C" "sh" "java" "js" "json" "clojure" "coffee"
             "C++" "css" "calc" "asymptote" "dot" "gnuplot" "ledger" "lilypond"
             "mscgen" "octave" "oz" "plantuml" "R" "sass" "screen" "sql" "awk"
             "ditaa" "haskell" "latex" "lisp" "matlab" "ocaml" "org" "perl" "ruby"
@@ -357,7 +357,9 @@ before packages are loaded. If you are unsure, you should try in setting them in
      (list (ido-completing-read "Source code type: " src-code-types))))
   (progn
     (newline-and-indent)
-    (insert (format "#+BEGIN_SRC %s\n" src-code-type))
+    (insert (if (not (string-equal src-code-type ""))
+                (format "#+BEGIN_SRC %s\n" src-code-type)
+                "#+BEGIN_SRC"))
     (newline-and-indent)
     (insert "#+END_SRC\n")
     (previous-line 2)
